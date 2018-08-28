480b1a888d21a832dd88496731137b7ddaa36a83
Michal Gebarowski <vmaaik@gmail.com>
2018-08-28 21:51:11 +0100

    add foodProvider service;

---
+++ src/main/java/com/gebarowski/probook/ch4/profiles/Food.java
@@ -0,0 +1,20 @@
+package com.gebarowski.probook.ch4.profiles;
+
+public class Food {
+    private String name;
+
+    public Food() {
+    }
+
+    public Food(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/gebarowski/probook/ch4/profiles/FoodProviderService.java
@@ -0,0 +1,9 @@
+package com.gebarowski.probook.ch4.profiles;
+
+import java.util.List;
+
+public interface FoodProviderService {
+
+    List<Food> provideLunchSet();
+
+}
