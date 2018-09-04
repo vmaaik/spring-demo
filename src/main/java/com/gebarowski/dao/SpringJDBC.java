d2ba9775fdcd38ef580d91fa281a9f06ca8c2f8a
Michal Gebarowski <vmaaik@gmail.com>
2018-09-04 20:03:16 +0100

    add student repository;

--- src/main/java/com/gebarowski/dao/SpringJDBC.java
+++ src/main/java/com/gebarowski/dao/SpringJDBC.java
@@ -2,16 +2,28 @@ package com.gebarowski.dao;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJDBC {
+public class SpringJDBC implements CommandLineRunner {
 
-        private Logger logger = LoggerFactory.getLogger(this.getClass());
-        public static void main(String[] args) {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-            SpringApplication.run(SpringJDBC.class, args);
+    @Autowired
+    StudentJdbcRepository repository;
 
-        }
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringJDBC.class, args);
+
+    }
+
+    @Override
+    public void run(String...args) throws Exception {
+        logger.info("Student id 10001 -> {}", repository.findById(10001L));
+    }
+
 }
--- src/main/java/com/gebarowski/dao/StudentJdbcRepository.java
+++ src/main/java/com/gebarowski/dao/StudentJdbcRepository.java
@@ -1,4 +1,20 @@
 package com.gebarowski.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class StudentJdbcRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+    public Student findById(long id) {
+        return jdbcTemplate.queryForObject("select * from student where id=?", new Object[]{
+                        id
+                },
+                new BeanPropertyRowMapper<Student>(Student.class));
+    }
 }
